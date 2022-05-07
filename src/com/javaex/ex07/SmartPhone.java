a40781a076dc7169ee640d51bbe16f82dee73249
dhqpqp1234 <dhqpqp1234@naver.com>
2022-05-07 15:49:19 +0900

    Practice 06

--- src/com/javaex/ex07/SmartPhone.java
+++ src/com/javaex/ex07/SmartPhone.java
@@ -2,22 +2,24 @@ package com.javaex.ex07;
 
 public class SmartPhone extends MusicPhone {
     
-    public void execute (String str){
-        
-        if("앱".equals(str)) {
-        	playApp();
-        }else {
-        	super.execute(str);
-        }
-        
-    }
- 
-    //메소드작성
-    
-    //메소드작성
-    protected void playApp(){
-        System.out.println("앱실행");
-    }
-    
-    
+	 public void execute (String str){
+	
+	        if("앱".equals(str)) {
+	        	playApp();
+	        }else {
+	        	super.execute(str);
+	        }
+	        
+	    }
+	 
+	    //메소드작성
+	    
+	    //메소드작성
+	    protected void playApp(){
+	        System.out.println("앱실행");
+	    }
+	    
+	    protected void playMusic(){
+	        System.out.println("다운로드해서 음악재생");
+	    }
 }
